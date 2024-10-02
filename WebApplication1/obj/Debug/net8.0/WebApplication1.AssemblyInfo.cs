fdfcd50e1d250719d3175d7ec34fc1aa2558269b
DevlinW98 <80415171+DevlinW98@users.noreply.github.com>
2024-10-03 06:00:40 +0700

    Update_Join_System

--- WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd433a7e277ab2b7bfa55bef43236887a4a01b9d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eeeca94f8c094ee173053120e25dd3dd7327cdd4")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
