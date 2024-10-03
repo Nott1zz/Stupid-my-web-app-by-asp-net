2de60439c26b9d38d4c8be3e77d65bf5edb26c2e
WongsatornS <66015173@kmitl.ac.th>
2024-10-03 15:57:49 +0700

    15:57

--- WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eeeca94f8c094ee173053120e25dd3dd7327cdd4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da72164e9a1eeb69c0e8f621cf9227fc98ea9509")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
