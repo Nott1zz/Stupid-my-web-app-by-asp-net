1a8628bb984e035b32da657f5e5d72cd03422bdd
Nott1zz <66015115@kmitl.ac.th>
2024-10-03 20:22:21 +0700

    20:22
    แก้ CSS หน้า : Home, Community

--- WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da72164e9a1eeb69c0e8f621cf9227fc98ea9509")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1bd3393601f18e19111a0329e0e0e1474a4ea0aa")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
